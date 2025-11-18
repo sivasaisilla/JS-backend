72c9619987a3de5e7a217852c669f07c47fb5f33
Sivasai <sivasai3920@gmail.com>
2025-11-18 10:31:29 +0530

    frontend and backend connection set

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react'
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(()=>{
+    axios.get('/api/jokes')
+    .then(response=>{
+      setJokes(response.data)
+    })
+    .catch(error=>{
+      console.error('Error fetching jokes:', error) 
+    })
+  },[jokes])
+
+  return (
+    <>
+      <h1>Hello Gooner</h1>
+      <p>JOKES : {jokes.length}</p>
+
+      {
+        jokes.map((joke,index)=> (
+          <div className={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
+    </>
+  )
+}
+
+export default App
